675912ca98f468229eb0295db27302092a3208ce
Maxim Fedyanin <101870811+LinInFal@users.noreply.github.com>
2024-11-02 17:20:36 +1000

    Update lab3.py

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -18,9 +18,9 @@ num = int(input("Введите число: "))
 fact = 1
 for num in range(2, num + 1):
     fact *= num
-    print(fact, end=' ')
+print(fact)
 
-print("\n=======Задание №5=======")
+print("=======Задание №5=======")
 x = 21
 while x > 0:
     x -= 1
@@ -32,9 +32,9 @@ n = int(input())
 print(fib1, fib2, end=' ')
 for i in range(2, n):
     fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end=' ')
+print(fib2)
 
-print("\n=======Задание №7=======")
+print("=======Задание №7=======")
 in_str = str(input("Введите строку: "))
 new_str = ""
 for i in range(len(in_str)):
